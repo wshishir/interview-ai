301ddfd8cc60a9ebc2f77e6880d710ed04e6ed14
Shishir <shishirtech506@gmail.com>
2025-02-02 03:23:09 +0530

    added clerk

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
