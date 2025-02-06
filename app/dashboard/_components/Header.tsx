777b22fea68c020cff1f3c23d7ddc13009a46492
shishir <shishirtech506@gmail.com>
2025-02-07 02:34:18 +0530

    changed orm

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,14 +1,41 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export function Header() {
-  return <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
-    <Image src={'/logo.png'} alt="logo" width={160} height={100}/>
-    <ul className="flex gap-6">
-        <li>Dashboard</li>
-        <li>FAQs</li>
-        <li>About</li>
-    </ul>
-    <UserButton/>
-  </div>;
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, []);
+  return (
+    <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
+      <Image src={"/logo.png"} alt="logo" width={160} height={100} />
+      <ul className="hidden md:flex gap-6">
+        <li
+          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+        ${path == '/dashboard' && "text-primary font-bold"}
+        `}
+        >
+          Dashboard
+        </li>
+        <li
+          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+        ${path == '/dashboard/faqs' && "text-primary font-bold"}
+        `}
+        >
+          FAQs
+        </li>
+        <li
+          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+        ${path == '/dashboard/about'  && "text-primary font-bold"}
+        `}
+        >
+          About us
+        </li>
+      </ul>
+      <UserButton />
+    </div>
+  );
 }
