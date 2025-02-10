6f11122696f8044c08bb27c4c1e882f71b6cc1ac
shishir <shishirtech506@gmail.com>
2025-02-11 04:02:46 +0530

    dialog box added

--- app/dashboard/_components/NewInterview.tsx
+++ app/dashboard/_components/NewInterview.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 const NewInterview = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>New</div>
+    <div>
+        <div className='p-10 border rounded-xl hover:scale-105 hover:shadow-md cursor-pointer bg-secondary transition-all' onClick={() => setOpen(true)}>
+            <h2 className='font-bold text-lg'>+ Add New</h2>
+        </div>
+        <Dialog open={open}>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+
+    </div>
+    
   )
 }
 
