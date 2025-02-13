a35daac5620955c5faae4a42d7a4da9a46b9b8cb
shishir <shishirtech506@gmail.com>
2025-02-14 05:21:08 +0530

    dialog box done

--- app/dashboard/_components/NewInterview.tsx
+++ app/dashboard/_components/NewInterview.tsx
@@ -1,6 +1,5 @@
-"use client"
-
-import { useState } from 'react'
+"use client";
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -8,40 +7,82 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 const NewInterview = () => {
   const [open, setOpen] = useState(false);
+  const [jobPosition, setJobPosition] = useState<string>("");
+  const [jobDesc, setJobDesc] = useState<string>("");
+  const [jobExperience, setJobExperience] = useState<number>(0);
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(jobPosition, jobDesc, jobExperience);
+  };
   return (
     <div>
-        <div className='p-10 border rounded-xl hover:scale-105 hover:shadow-md cursor-pointer bg-secondary transition-all' onClick={() => setOpen(true)}>
-            <h2 className='font-bold text-lg'>+ Add New</h2>
-        </div>
-        <Dialog open={open}>
-  <DialogContent className='bg-white'>
-    <DialogHeader>
-      <DialogTitle>Tell us more about your Job Interview</DialogTitle>
-      <DialogDescription>
-        <div>
-          <h2>Add details about the job position</h2>
-          <div>
-            <label>Job Position</label>
-            <Input placeholder="Frontend Developer"/>
-          </div>
-        </div>
-      <div className='flex gap-5 justify-end'>
-          <Button variant="ghost" onClick={() => setOpen(false)}>Cancel</Button>
-          <Button>Start Interview</Button>
-        </div>
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
+      <div
+        className="p-10 border rounded-xl hover:scale-105 hover:shadow-md cursor-pointer bg-secondary transition-all"
+        onClick={() => setOpen(true)}
+      >
+        <h2 className="font-bold text-lg">+ Add New</h2>
+      </div>
+      <Dialog open={open}>
+        <DialogContent className="bg-white">
+          <DialogHeader>
+            <DialogTitle>Tell us more about your Job Interview</DialogTitle>
+            <DialogDescription asChild>
+              <form onSubmit={onSubmit}>
+                <div>
+                  <h2>Add details about the job position</h2>
+                  <div className="mt-7 my-3">
+                    <label>Job Position</label>
+                    <Input
+                      placeholder="Frontend Developer"
+                      required
+                      onChange={(event) => setJobPosition(event.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label>Job Description</label>
+                    <Textarea
+                      onChange={(event) => setJobDesc(event.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label>Years of Experience</label>
+                    <Input
+                      placeholder="5"
+                      type="number"
+                      max={10}
+                      min={0}
+                      required
+                      onChange={(event) =>
+                        setJobExperience(Number(event.target.value))
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    onClick={() => setOpen(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button type="submit">Start Interview</Button>
+                </div>
+              </form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-    
-  )
+  );
 };
 
-export default NewInterview
+export default NewInterview;
