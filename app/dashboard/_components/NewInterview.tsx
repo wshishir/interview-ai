e805665c94150b29f7e2c8d8f1e756bf3e152df3
shishir <shishirtech506@gmail.com>
2025-02-18 14:50:14 +0530

    completed dialog

--- app/dashboard/_components/NewInterview.tsx
+++ app/dashboard/_components/NewInterview.tsx
@@ -7,10 +7,12 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogFooter,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 const NewInterview = () => {
   const [open, setOpen] = useState(false);
@@ -24,61 +26,45 @@ const NewInterview = () => {
   };
   return (
     <div>
-      <div
-        className="p-10 border rounded-xl hover:scale-105 hover:shadow-md cursor-pointer bg-secondary transition-all"
-        onClick={() => setOpen(true)}
-      >
-        <h2 className="font-bold text-lg">+ Add New</h2>
-      </div>
-      <Dialog open={open}>
-        <DialogContent className="bg-white">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="rounded-lg">
+            New Interview
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px] ">
           <DialogHeader>
-            <DialogTitle>Tell us more about your Job Interview</DialogTitle>
-            <DialogDescription asChild>
-              <form onSubmit={onSubmit}>
-                <div>
-                  <h2>Add details about the job position</h2>
-                  <div className="mt-7 my-3">
-                    <label>Job Position</label>
-                    <Input
-                      placeholder="Frontend Developer"
-                      required
-                      onChange={(event) => setJobPosition(event.target.value)}
-                    />
-                  </div>
-                  <div className="my-3">
-                    <label>Job Description</label>
-                    <Textarea
-                      onChange={(event) => setJobDesc(event.target.value)}
-                    />
-                  </div>
-                  <div className="my-3">
-                    <label>Years of Experience</label>
-                    <Input
-                      placeholder="5"
-                      type="number"
-                      max={10}
-                      min={0}
-                      required
-                      onChange={(event) =>
-                        setJobExperience(Number(event.target.value))
-                      }
-                    />
-                  </div>
-                </div>
-                <div className="flex gap-5 justify-end">
-                  <Button
-                    type="button"
-                    variant="ghost"
-                    onClick={() => setOpen(false)}
-                  >
-                    Cancel
-                  </Button>
-                  <Button type="submit">Start Interview</Button>
-                </div>
-              </form>
+            <DialogTitle>Tell me more about your job </DialogTitle>
+            <DialogDescription>
+              Add details about the job position, job description and your years
+              of experience
             </DialogDescription>
           </DialogHeader>
+          <div className="gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Job Position
+              </Label>
+              <Input id="name" className="col-span-3" required />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Job Description
+              </Label>
+              <Textarea id="username" className="col-span-3" required />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Years of Experience
+              </Label>
+              <Input id="username" className="col-span-3" max={20} />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit" className="bg-black hover:bg-black/80">
+              Start Interview
+            </Button>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
